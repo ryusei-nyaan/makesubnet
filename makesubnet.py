8b0b9b14aa44f3f1a96fae8ddbba24cd26b80cfc
ryusei-nyaan <r.s.iwasaki@ezweb.ne.jp>
2024-01-02 15:04:22 +0900

    second commit

--- makesubnet.py
+++ makesubnet.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def landing():
     #ランディングページの処理
@@ -21,28 +22,50 @@ def showing():
 
         #最初のIP
         for i in range(1,5):
+
             start_ip += request.form[str(i) + "octet_start"]
+
             if i != 4:
                 start_ip += "."
 
-        start_ip = ipaddress.IPv4Address(start_ip)
+        try:
+            start_ip = ipaddress.IPv4Address(start_ip)
+        except:
+            return redirect(url_for("relanding"))
+
+
 
         #最後のIP
         for i in range(1,5):
             end_ip += request.form[str(i) + "octet_end"]
+                
             if i != 4:
                 end_ip += "."
         
-        end_ip = ipaddress.IPv4Address(end_ip)
+        try:
+            end_ip = ipaddress.IPv4Address(end_ip)
+        except:
+            return redirect(url_for("relanding"))
 
+
+        
         if start_ip <= end_ip:
             ip_range = ipaddress.summarize_address_range(start_ip,end_ip)
         else:
-            return redirect(url_for("landing"))
+            return redirect(url_for("relanding"))
+
 
 
         return render_template("index2.html",parameter=ip_range)
 
 
+@app.route("/retry", methods=["GET"])
+def relanding():
+    #ランディングページの処理
+    if request.method == "GET":
+        return render_template("index3.html")
+
+
+
 if __name__ == "__main__":
-    app.run(port="8888")
+    app.run(port="8888",debug=False)
